da01a58c9811b2aff227d6f84f634d3993d4c5d2
д а <ilya.belonojko@gmail.com>
2025-04-25 17:10:55 +0400

    facedataset при признаках Хаара зависает

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import DataReduction
 
 
 def demonstrate_haar():
-    images_path, labels_path = DataReduction.reduct_images_dataset("C:\\Users\\ilyab\\Downloads\\wider_face\\images",
-                                                                   "C:\\Users\\ilyab\\Downloads\\wider_face\\labels", 100)
+    images_path, labels_path = DataReduction.reduct_images_dataset("wider_face/images",
+                                                                   "wider_face/labels", 100)
 
     if not os.path.isfile('haar_trained.pkl'):
         model = HaarBasedMethods.get_trained_model(images_path, labels_path)
@@ -17,14 +17,14 @@ def demonstrate_haar():
         model = joblib.load('haar_trained.pkl')
         print("Модель десериализована")
 
-    HaarBasedMethods.detect_and_draw("C:\\Users\\ilyab\\Downloads\\testImage2.jpg",
-                               model, "C:\\Users\\ilyab\\PycharmProjects\\kursach\\ProcessedImages\\testImageHaar.jpg")
+    HaarBasedMethods.detect_and_draw("wider_face/images/wider_2236.jpg",
+                               model, "ProcessedImages/testImageHaar.jpg")
     print("Обнаружены лица на тестовом изображении")
 
 def demonstrate_hog():
     images_path, labels_path = DataReduction.reduct_images_dataset(
-        "C:\\Users\\ilyab\\Downloads\\wider_face\\images",
-        "C:\\Users\\ilyab\\Downloads\\wider_face\\labels", 100)
+        "wider_face/images",
+        "wider_face/labels", 100)
 
     if not os.path.isfile('hog_trained.pkl'):
         model = HogMethods.get_trained_model(images_path, labels_path)
@@ -34,12 +34,12 @@ def demonstrate_hog():
         model = joblib.load('hog_trained.pkl')
         print("Модель Hog десериализована")
 
-    HogMethods.detect_and_draw("C:\\Users\\ilyab\\Downloads\\testImage2.jpg",
-                               model, "C:\\Users\\ilyab\\PycharmProjects\\kursach\\ProcessedImages\\testImageHog.jpg")
+    HogMethods.detect_and_draw("wider_face/images/wider_2236.jpg",
+                               model, "ProcessedImages/testImageHog.jpg")
     print("Обнаружены лица на тестовом изображении")
 
 
 
 if __name__ == '__main__':
-    demonstrate_hog()
+    # demonstrate_hog()
     demonstrate_haar()
